6df3e6ee15ad45e2217455caad4112bce9dd11f9
SoWeBegin <36608065+SoWeBegin@users.noreply.github.com>
2025-09-15 00:19:51 +0200

    Update MainEquippedItem.h

--- MainServer/include/Structures/Item/MainEquippedItem.h
+++ MainServer/include/Structures/Item/MainEquippedItem.h
@@ -15,7 +15,7 @@ namespace Main
 #ifdef _WIN32
 		using ExpirationTimeType = __time32_t;
 #else
-		using ExpirationTimeType = int32_t;
+		using ExpirationTimeType = std::int32_t;
 #endif
 
 PACK_PUSH(1)
