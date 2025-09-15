f9cc65a430547ad9dfd580c8dc991732ad584c9c
SoWeBegin <36608065+SoWeBegin@users.noreply.github.com>
2025-09-15 19:13:52 +0200

    Update RoomCreationHandler.h

--- MainServer/include/Handlers/Room/RoomCreationHandler.h
+++ MainServer/include/Handlers/Room/RoomCreationHandler.h
@@ -79,8 +79,9 @@ namespace Main
 				response.setExtra(RoomCreationExtra::CREATION_FAIL);
 				session->asyncWrite(response);
 			}
-			else if (Main::Ipc::M2C_sendRoomNumber(session->getId(), room.getRoomNumber()))
+			else
 			{
+				Main::Ipc::M2C_sendRoomNumber(session->getId(), room.getRoomNumber());
 				room.setStateFor(session->getAccountInfo().uniqueId, Common::Enums::STATE_WAITING);
 				session->setRoomNumber(room.getRoomNumber());
 				const std::pair<std::uint16_t, std::uint16_t> roomInfo{ room.getRoomNumber() - 1, 2 }; // {roomNum, unk}
@@ -97,12 +98,6 @@ namespace Main
 					session->asyncWrite(response);
 				}
 				roomsManager.addRoom(std::move(room));
-			}
-			else
-			{
-				response.setExtra(RoomCreationExtra::CREATION_FAIL);
-				session->asyncWrite(response);
-				session->sendMessage("[handleRoomCreation] Could not retrieve cast IPC ACK related to room number");
 			}
 
 			END_BENCHMARK(handleRoomCreation, session)
